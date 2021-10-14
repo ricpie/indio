6b608db56bcb808125b31aafe668ddbfe8a665ee
ricpie <ricpie@gmail.com>
2021-10-14 14:32:19 -0600

    new RMDs

--- r_scripts/load.R
+++ r_scripts/load.R
@@ -1,7 +1,7 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(lubridate,plyr,dplyr,reshape2,devtools,shiny,shinydashboard,dygraphs,DT,shinyjs,tools,data.table,writexl,zoo,readxl
 ,gmailr,mailR,cronR,miniUI,shinyFiles,ggplot2,stringr,chron,doParallel,foreach,openxlsx,gridExtra,egg,cowplot,corrgram,
-factoextra,scales,htmlwidgets,tidyfast,tidyr,kableExtra,janitor)
+factoextra,scales,htmlwidgets,tidyfast,tidyr,kableExtra,janitor,xlsx,FuzzyNumbers,tibble,openair,npregfast)
 
 registerDoParallel(cores=detectCores()-2)
 
